a15285ae39ee3927dd91d6696aa9da2ca67191fc
mayiwen <i@mayiwen.com>
2023-10-29 21:42:34 +0800

    2023-10-29

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '这是马一文的后端服务!主页是http://mayiwen.com';
+    return `这是马一文的后端服务!主页是
+    
+    <a href="http://mayiwen.com">http://mayiwen.com<a/>
+    `;
   }
 }
